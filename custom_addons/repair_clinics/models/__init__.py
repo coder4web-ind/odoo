1bca878aa3d0f2aaf9a0d493f3a191e648110a4e
coder4web-ind <coder4web@yahoo.com>
2026-07-09 00:25:17 +0530

    Security cleanup: remove proprietary modules from public branch

--- custom_addons/repair_clinics/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import device_brand
-from . import device_category
-from . import device
-from . import repair_job
--- custom_addons/repair_device/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import manufacturer
-from . import device_unit_type
-from . import device_model
